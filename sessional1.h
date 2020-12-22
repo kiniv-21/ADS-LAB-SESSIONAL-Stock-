9c1e8fdd9327b433ac1e364bcbe3d936a2dc1bdd
kiniv-21 <42902325+kiniv-21@users.noreply.github.com>
2020-12-22 10:28:06 +0530

    Add files via upload

---
+++ sessional1.h
@@ -0,0 +1,38 @@
+#ifndef SESSIONAL1_H_INCLUDED
+#define SESSIONAL1_H_INCLUDED
+#include "appconst.h"
+typedef struct _trading_queue_ TradingQ;
+
+struct _trading_queue_ {
+    char name[40];
+    char date[20];
+    int32_t price;
+    int32_t quantity;
+};
+
+typedef struct _queue_ Queue;
+
+struct _queue_ {
+    uint32_t size;
+    uint32_t count;
+    uint32_t rear;
+    uint32_t front;
+    int32_t data[Q_LEN];
+};
+
+typedef struct _queue_result_ QueueResult;
+
+struct _queue_result_ {
+    TradingQ share;
+    uint32_t status;
+};
+
+ Queue queue_neww(uint32_t size);
+ uint8_t queue_full(Queue 'q');
+ uint8_t queue_empty(Queue 'q');
+ Queue' queue_add(Queue 'q, TradingQ share, QueueResult 'res);
+ Queue' queue_delete(Queue 'q, QueueResult 'res);
+ Queue' look_up(Queue 'q, TradingQ share);
+ Queue' topup(Queue 'q, TradingQ share, int32_t quantity);
+ uint32_t queue_length(Queue 'q);
+#endif // SESSIONAL1_H_INCLUDED
